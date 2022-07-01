74e67ce6868e4737ac0ed1e73d033910b713c386
chethanrinteger <108517131+chethanrinteger@users.noreply.github.com>
2022-07-01 18:29:09 +0530

    Update da.py

--- da.py
+++ da.py
@@ -14,5 +14,4 @@ st.sidebar.markdown("Inventory And Product Performance")
 DATA_URL = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vRffwlmhiARpcl03gaG07TDMbFCbjJOdFpOkscij4-CPIwzNCiQdwC8TsxBjKjN6zV3BUraUDjxUC0p/pub?gid=1604835951&single=true&output=csv")
 data = pd.read_csv(DATA_URL)
 #data = load_data()
-
 st.write(data)
